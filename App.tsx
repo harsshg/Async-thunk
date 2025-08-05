9e21d7a2486c285f6bc345fa415257ffe8a5c856
harsshg <harshgupta1.hp@gmail.com>
2025-08-05 17:32:22 +0530

    Added some stylings

--- App.tsx
+++ App.tsx
@@ -32,9 +32,9 @@ const ProductsList = () => {
       keyExtractor={(item) => item.id.toString()}
       renderItem={({ item }) => (
         <View style={{alignItems:'center'}} >
-        <View style={{margin: 8,height:400,width:400,backgroundColor:'rgb(255, 255, 255)',alignItems:'center',padding:10,borderWidth:2,borderRadius:30}}>
+        <View style={{margin: 8,height:400,width:400,backgroundColor:'rgb(255, 255, 255)',alignItems:'center',padding:10,borderWidth:4,borderRadius:30,borderColor:'rgba(0, 0, 0, 0.27)'}}>
           <Image source={{ uri: item.image }} style={{ width: 200, height: 200, marginRight: 10 }} />
-          <View style={{marginTop:13}}>
+          <View style={{marginTop:10,backgroundColor:"rgba(0, 0, 0, 0.08)",padding:10,borderRadius:15}}>
             <Text style={{color:'rgba(0, 0, 0, 0.7)',fontSize:20,fontWeight:'500'}}>{item.title}</Text>
             <Text style={{color:'red',fontWeight:'500',fontSize:20}}>${item.price}</Text>
           </View>
