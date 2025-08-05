18cad7c96927a8367b7730070072d49427da7cf5
harsshg <harshgupta1.hp@gmail.com>
2025-08-05 14:26:45 +0530

    initial

--- android/app/src/features/userdetailslice.js
+++ android/app/src/features/userdetailslice.js
@@ -1,14 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { userAPI } from './userAPI'
 
-export const userDetail = createSlice({
-    name:'undefined',
-    initialState:{
-        user : [],
-        loading:false,
-        error:null,
-    }
+// First, create the thunk
+const fetchUserById = createAsyncThunk(
+  'users/fetchByIdStatus',
+  async (userId, thunkAPI) => {
+    const response = await userAPI.fetchById(userId)
+    return response.data
+  },
+)
 
+/**
+ * @typedef {Object} UsersState
+ * @property {Array} entities
+ * @property {'idle' | 'pending' | 'succeeded' | 'failed'} loading
+ */
 
+const initialState = {
+  entities: [],
+  loading: 'idle',
+}
+
+// Then, handle actions in your reducers:
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    // standard reducer logic, with auto-generated action types per reducer
+  },
+  extraReducers: (builder) => {
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder.addCase(fetchUserById.fulfilled, (state, action) => {
+      // Add user to the state array
+      state.entities.push(action.payload)
+    })
+  },
 })
 
-export default userDetail.reducer;
+// Later, dispatch the thunk as needed in the app
+dispatch(fetchUserById(123))
