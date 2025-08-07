1333dc5fa7adb7340efec1733adc43da9b4e2974
harsshg <harshgupta1.hp@gmail.com>
2025-08-07 11:50:50 +0530

    Changed all import and export in index.js files

--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,22 @@
-export {default as Detail} from './Detail';
-export {default as ProductList} from './productList';
-export {default as Header} from './Header';
+// export {default as Detail} from './Detail';
+// export {default as ProductList} from './productList';
+// export {default as Header} from './Header';
+
+
+import Detail from './Detail';
+
+export {
+Detail
+}
+
+import ProductList from './productList';
+
+export {
+ProductList
+}
+
+import Header from './Header';
+
+export {
+Header
+}
